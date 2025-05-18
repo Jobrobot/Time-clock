2809fa984aa60d872e61bf8fa6f140d8e53a43ea
Jahangir Huseynov <Jahnigaming@gmail.com>
2025-05-18 15:30:08 -0700

    init

---
+++ main.js
@@ -0,0 +1,38 @@
+const strips = [...document.querySelectorAll(".strip")];
+const numberSize = 8;
+
+function highlight(strip, d) {
+  strips[strip]
+    .querySelector(`.number:nth-of-type(${d + 1})`)
+    .classList.add("pop");
+
+  setTimeout(() => {
+    strips[strip]
+      .querySelector(`.number:nth-of-type(${d + 1})`)
+      .classList.remove("pop");
+  }, 950);
+}
+
+function stripSplider(strip, number) {
+  let d1 = Math.floor(number / 10);
+  let d2 = number % 10;
+
+  strips[strip].style.transform = `translateY(${d1 * -numberSize}vmin)`;
+  highlight(strip, d1);
+  if (strip + 1 < strips.length) {
+    strips[strip + 1].style.transform = `translateY(${d2 * -numberSize}vmin)`;
+    highlight(strip + 1, d2);
+  }
+}
+
+setInterval(() => {
+  const time = new Date();
+
+  const hours = time.getHours();
+  const mins = time.getMinutes();
+  const secs = time.getSeconds();
+
+  stripSplider(0, hours);
+  stripSplider(2, mins);
+  stripSplider(4, secs);
+}, 1000);
